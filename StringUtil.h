0356a9fa4d35ea2733f1b3fb06d0d8642966e059
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2015-05-19 16:25:02 +0200

    Update StringUtil.h

--- StringUtil.h
+++ StringUtil.h
@@ -11,7 +11,8 @@ public:
 
     static QString toSnakeCase(const QString &str) {
         QString snake_str = str;
-        return snake_str.replace(QRegExp("(.)([A-Z][a-z]+)"), "\\1_\\2").replace(QRegExp("([a-z0-9])([A-Z])"), "\\1_\\2").toLower();
+        return snake_str.replace(QRegExp("(.)([A-Z][a-z]+)"), "\\1_\\2")
+                        .replace(QRegExp("([a-z0-9])([A-Z])"), "\\1_\\2").toLower();
     }
 
     static QString toCamelCase(const QString &str) {
